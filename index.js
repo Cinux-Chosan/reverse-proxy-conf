1d80b59f38d6f813f8f951fcf6e29a1c87505833
Chosan <179817004@qq.com>
2018-04-06 12:01:52 +0800

    初步完成， 第一次推上线测试

---
+++ index.js
@@ -0,0 +1,44 @@
+const http = require('http');
+const https = require('https');
+const httpProxy = require('http-proxy');
+const fs = require('fs');
+
+let { URL } = require('url');
+
+let  cert = fs.readFileSync('/etc/letsencrypt/live/www.chosan.cn/fullchain.pem'),
+key = fs.readFileSync('/etc/letsencrypt/live/www.chosan.cn/privkey.pem')
+
+let proxyOptions = {
+    ssl: {
+        cert,
+        key
+    },
+    secure: true
+}
+
+let httpsOptions = {
+    cert,
+    key
+}
+
+let proxy = httpProxy.createProxyServer(proxyOptions);
+
+https.createServer(httpsOptions, (req, res) => {
+    let url = req.url;
+
+    if (url.startsWith('/wxapi')) {
+        proxy.web(req, res, { target: 'https://localhost:9000' });  // 9000 用作 wxapi 端口
+    } 
+    if (url.startsWith('/api')) {
+        proxy.web(req, res, { target: 'https://localhost:3000' });  // 3000 用作博客端口
+    }
+
+}).listen(443)
+
+
+// 负责将 http 请求重定向到 https
+http.createServer((req, res) => { 
+    let redirectUrl = new URL(req.url, `https://${req.headers.host}`);
+    res.writeHead(301, { 'Location': redirectUrl.toString() });
+    res.end();
+}).listen(80);
