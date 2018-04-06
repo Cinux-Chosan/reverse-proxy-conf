2065b5b088c59878802142d3bf5448372882a8c6
Chosan <179817004@qq.com>
2018-04-06 12:15:54 +0800

    初步完成， 第一次推上线测试

--- index.js
+++ index.js
@@ -26,6 +26,8 @@ let proxy = httpProxy.createProxyServer(proxyOptions);
 https.createServer(httpsOptions, (req, res) => {
     let url = req.url;
 
+    console.log('https', req.headers.host, req.url);
+
     if (url.startsWith('/wxapi')) {
         proxy.web(req, res, { target: 'https://localhost:9000' });  // 9000 用作 wxapi 端口
     } 
@@ -40,6 +42,7 @@ https.createServer(httpsOptions, (req, res) => {
 
 // 负责将 http 请求重定向到 https
 http.createServer((req, res) => { 
+    console.log('http', req.headers.host, req.url);
     let redirectUrl = new URL(req.url, `https://${req.headers.host}`);
     res.writeHead(301, { 'Location': redirectUrl.toString() });
     res.end();
