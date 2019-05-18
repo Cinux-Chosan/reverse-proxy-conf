2d2d14bc17c250c5e6d109c022632b59cb17be88
chosan <179817004@qq.com>
2019-05-18 19:19:20 +0800

    添加数据请求缓存

--- index.js
+++ index.js
@@ -5,71 +5,69 @@ const fs = require("fs");
 const spdy = require("spdy");
 const request = require('request');
 
-let { URL } = require("url");
+const { URL } = require("url");
 
-let util = require("util");
+const util = require("util");
+const relay = require('./lib/relay');
 
-let cert = fs.readFileSync("/etc/letsencrypt/live/chosan.cn/fullchain.pem"),
-  key = fs.readFileSync("/etc/letsencrypt/live/chosan.cn/privkey.pem");
+const proxy = httpProxy.createProxyServer({});
+// const httpsProxy = httpProxy.createProxyServer();
 
-let proxyOptions = {
-  ssl: {
-    cert,
-    key
-  },
-  secure: true
-};
+// const cert = fs.readFileSync("/etc/letsencrypt/live/chosan.cn/fullchain.pem"),
+//   key = fs.readFileSync("/etc/letsencrypt/live/chosan.cn/privkey.pem");
 
-let httpsOptions = {
-  cert,
-  key
-};
+// const proxyOptions = {
+//   ssl: {
+//     cert,
+//     key
+//   },
+//   secure: true
+// };
 
-let proxy = httpProxy.createProxyServer({});
-let httpsProxy = httpProxy.createProxyServer();
+// const httpsOptions = {
+//   cert,
+//   key
+// };
 
-// https
-spdy
-  .createServer(httpsOptions, (req, res) => {
-    let host = req.headers.host;
-    let url = req.url;
 
-    console.log("https request\n", host, url);
 
-    let toUrl = new URL(url, `https://${host}`).searchParams.get('toUrl');
-    if (toUrl) {
-      // 有 toUrl 参数则代表代理到 toUrl
-        console.log('代理到 toUrl');
-        toUrl = decodeURIComponent(toUrl);
-        console.log('toUrl', toUrl);
-        return req.pipe(request(toUrl)).pipe(res);
-    }
-    try {
-      switch (host) {
-        case "wx.chosan.cn":
-          httpsProxy.web(req, res, { target: "http://localhost:9000" }); // 9000 用作 wxapi 端口
-          break;
-        case "mobile.chosan.cn":
-          httpsProxy.web(req, res, { target: "http://localhost:9001" }); // 9001 用作测试 app-mobile
-          break;
-        case "xtoken.ren":
-        case "angel.xtoken.ren":
-          httpsProxy.web(req, res, { target: "http://localhost:60001" }); // 60001 用作 xtoken 端口
-          break;
-        case "chosan.cn":
-        case "www.chosan.cn":
-          httpsProxy.web(req, res, { target: "https://localhost:3000" }); // 3000 用作博客端口
-          break;
-        default:
-          break;
-      }
-    } catch (error) {
-      console.error(error)
-    }
-  })
-  .listen(443, () => {
-    console.log("443端口启动成功！");
-  });
+// // https
+// spdy
+//   .createServer(httpsOptions, (req, res) => {
+//     const host = req.headers.host;
+//     const url = req.url;
+
+//     console.log("https request\n", host, url);
+
+//     if (relay.isRelay(req)) {
+//       return relay(req, res);
+//     }
+//     try {
+//       switch (host) {
+//         case "wx.chosan.cn":
+//           httpsProxy.web(req, res, { target: "http://localhost:9000" }); // 9000 用作 wxapi 端口
+//           break;
+//         case "mobile.chosan.cn":
+//           httpsProxy.web(req, res, { target: "http://localhost:9001" }); // 9001 用作测试 app-mobile
+//           break;
+//         case "xtoken.ren":
+//         case "angel.xtoken.ren":
+//           httpsProxy.web(req, res, { target: "http://localhost:60001" }); // 60001 用作 xtoken 端口
+//           break;
+//         case "chosan.cn":
+//         case "www.chosan.cn":
+//           httpsProxy.web(req, res, { target: "https://localhost:3000" }); // 3000 用作博客端口
+//           break;
+//         default:
+//           break;
+//       }
+//     } catch (error) {
+//       console.error(error)
+//     }
+//   })
+//   .listen(443, () => {
+//     console.log("443端口启动成功！");
+//   });
 
 proxyMap = new Map();
 proxyMap.set("ysd.kim", "http://www.atool.org");
@@ -80,30 +78,26 @@ proxyMap.set("mln.kim", "http://www.a5ks.com");
 // 负责将 http 请求重定向到 https
 http
   .createServer((req, res) => {
-    let host = req.headers.host;
-    let url = req.url;
+    const host = req.headers.host;
+    const url = req.url;
     console.log("http request\n", host, url, req.headers);
-    let toUrl = new URL(url, `http://${host}`).searchParams.get('toUrl');
 
     let key = "";
     if ((key = [...proxyMap.keys()].find(el => host.includes(el)))) {
       console.log('代理到其他网站');
-      let target = proxyMap.get(key);
-      let hostIndex = req.rawHeaders.findIndex(
+      const target = proxyMap.get(key);
+      const hostIndex = req.rawHeaders.findIndex(
         el => el.toLowerCase() === "host"
       );
       req.headers.host = req.rawHeaders[hostIndex + 1] = target.replace(/(http|https):\/\//, "");
       console.log(`重定向到\t${target}`);
       proxy.web(req, res, { target });
-    } else if (toUrl) {
-      // 有 toUrl 参数则代表代理到 toUrl
-        console.log('代理到 toUrl');
-        toUrl = decodeURIComponent(toUrl);
-        console.log('toUrl', toUrl);
-        req.pipe(request(toUrl)).pipe(res);
+    } else if (relay.isRelay(req)) {
+      // 需要做请求代理
+      relay(req, res);
     } else {
       console.log('重定向到 https');
-      let redirectUrl = new URL(url, `https://${host}`);
+      const redirectUrl = new URL(url, `https://${host}`);
       req.headers.origin &&
         res.setHeader("Access-Control-Allow-Origin", req.headers.origin);
       res.writeHead(301, { Location: redirectUrl.toString() });
